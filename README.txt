9ea43d6ebd04b152148a520279532cea75187254
darrenho1994 <darkdrium_1994@hotmail.com>
2021-10-12 18:26:15 +0800

    y

--- README.txt
+++ README.txt
@@ -5,3 +5,20 @@ docker push darrenho1994/bms-flask
 kubectl config set-context --current --namespace=smu-team07 
 
 kubectl apply -f ./deployment.yaml
+
+
+How to access pod files:
+1. kubectl exec --stdin --tty capexample-app-58d8cbb99c-n8d9f -- /bin/bash 
+
+2. type redis-cli
+
+3. auth kPppOZp2hC
+
+4. select 0
+
+
+keys * (to search alll keys in my db)
+
+FLUSHALL (to DELETE KEYS FROM ALL DATABASE)
+
+FLUSHDB (to delete keys of the selected Redis Database) < use this
--- strong_wave_event.py
+++ strong_wave_event.py
@@ -1,16 +1,86 @@
 import os
 import redis
 import json
+import requests
+from requests.auth import HTTPBasicAuth
 
+def getBearerToken(authUrl, clientId, clientSecret):
+    callUrl = authUrl
+    request_body = {'grant_type': 'client_credentials', 'response_type': 'token', 'client_id': clientId, 'client_secret': clientSecret}
+    response = requests.post(callUrl, data = request_body)
+    print("Bearer token request status from server: " + str(response.status_code))
+    data = json.loads(response.text)
+    bToken = data["access_token"]
+    return bToken
+# Get (almost) all data of the "thing"
+def getThingData(iotThingUrl, bToken, thingId):
+    print("ThingID: " + thingId)
+    callUrl = iotThingUrl + thingId + "')"
+    headers = {'Authorization': 'Bearer ' + bToken}
+    response = requests.get(callUrl, headers = headers)
+    print("Thing data request status from server: " + str(response.status_code))
+    data = json.loads(response.text)
+    asg = data['_assignment']
+    device = asg['_devices']
+    for i in device:
+        deviceId = i['_id']
+        for j in i['_sensors']:
+            sensorId = j['_id']
+            sensorTypeId = j['_sensorTypeId']
+    return (deviceId, sensorId, sensorTypeId)    
+
+# Retrieve capabilities from SAP IoT device management (remaining data needed)
+def getThingCapabilities(iotUrl, iotUser, iotPassword, iotTenant, sensorType):
+    callUrl = iotUrl + iotTenant + "/sensorTypes/" + sensorType 
+    response = requests.get(callUrl, auth = HTTPBasicAuth(iotUser, iotPassword))
+    print("Capabilities request status from server: " + str(response.status_code))
+    data = json.loads(response.text)
+    capabilityList = data["capabilities"]
+    commandCapabilityId = ""
+    for capability in data["capabilities"]:
+        if capability["type"] == "command":
+            commandCapabilityId = capability["id"]
+    return commandCapabilityId        
 
 def main(event, context):
-
-    # redisHost = 'localhost'
-    # redisPort = '6379'
-    # redisPassword = ''
-    # redisDb = 0
-        # r = redis.Redis(host=redisHost, port = redisPort, db=int(redisDb), password=redisPassword, socket_timeout=None)
+    clientSecret = os.getenv('CLIENTSECRET')
+    clientId = os.environ.get('CLIENTID')
+    authUrl = os.environ.get('AUTHENTICATIONURL')
+    iotUrl = os.environ.get('IOTMGMTURL')
+    iotUser = os.environ.get('IOTUSER')
+    iotPassword = os.environ.get('IOTPASSWORD')
+    iotTenant = os.environ.get('IOTTENANT')
+    iotThingUrl = os.environ.get('IOTTHINGURL')
+    redisHost = os.environ.get('REDISHOST')
+    redisPort = os.environ.get('REDISPORT')
+    redisPassword = os.environ.get('REDISPASSWORD')
+    redisDb = os.environ.get('REDISDBNO')    
+    r = redis.Redis(host=redisHost, port = redisPort, db=int(redisDb), password=redisPassword, socket_timeout=None, decode_responses=True)
+    #event = {'data': {'thingId': '38ED5BF550EE4CC6AD2BE9A7BE7111A4', 'thingCoordinate': '1.3050856285437102, 103.93210128266621'}}
     thingId = event["data"]["thingID"]
-    # we want to use this thingId to get  device id, sensor id etc.. so can store it in redis. (continue coding from here later)
-    print(thingId)
+    coordinate = event["data"]["thingCoordinate"]
 
+    bearerToken = getBearerToken(authUrl, clientId, clientSecret)
+    (deviceId, sensorId, sensorTypeId) = getThingData(iotThingUrl, bearerToken, thingId)
+    commandCapabilityId = getThingCapabilities(iotUrl, iotUser, iotPassword, iotTenant, sensorTypeId)
+    dataset = {"thingId": thingId, "deviceId": deviceId, "sensorId": sensorId, "commandCapabilityId": commandCapabilityId, "coordinate": coordinate }
+
+    deviceFound = 0
+    for device in r.lrange('devices', 0, -1):
+        device = json.loads(device)
+        if device['thingId'] == thingId:
+            deviceFound = deviceFound + 1          
+
+    #We check if this Thing ID and the Thing Data are stored in Redis, if not, we push. If already exist, we check if anything else has changed, such as the coordinates, then we delete old data and replace it with new data.
+    if deviceFound == 0:
+        r.rpush('devices', json.dumps(dataset))
+    else:
+        # dataset = {"thingId": thingId, "deviceId": deviceId, "sensorId": sensorId, "commandCapabilityId": commandCapabilityId, "coordinate": "1314" }
+        for ind in range(len(r.lrange('devices', 0, -1))):
+            device = json.loads(r.lrange('devices', 0, -1)[ind])
+            if device['thingId'] == thingId:
+                if device != dataset:
+                    r.lrem('devices', 1, r.lrange('devices', 0, -1)[ind])
+                    r.rpush('devices', json.dumps(dataset))
+
+    
