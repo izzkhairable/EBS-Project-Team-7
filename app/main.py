1cf7920043c3bbe842668c657e6ee9e2dc784052
izzkhairable <izzkhair@gmail.com>
2021-10-17 05:18:02 +0800

    Finished the homepage, still require refactoring

--- app/main.py
+++ app/main.py
@@ -1,31 +1,91 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 import redis
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
 
-
-@app.route("/noob")
-def noob():
-    # redisHost = 'localhost'
+@app.route("/", methods=["GET"])
+def main():
+    # redisHost = 'redis'
     # redisPort = '6379'
-    # redisPassword = ''
-    # redisDb = 0
-    redisHost = 'redis'
-    redisPort = '6379'
-    redisPassword = 'kPppOZp2hC'
-    redisDb = 5   
-    r = redis.Redis(host=redisHost, port = redisPort, db=int(redisDb), password=redisPassword, socket_timeout=None, decode_responses=True)
+    # redisPassword = 'kPppOZp2hC'
+    # redisDb = 5
+
+    redisHost = "localhost"
+    redisPort = "6379"
+    redisPassword = ""
+    redisDb = 0
+
+    r = redis.Redis(
+        host=redisHost,
+        port=redisPort,
+        db=int(redisDb),
+        password=redisPassword,
+        socket_timeout=None,
+        decode_responses=True,
+    )
     r.flushall()
-    r.lpush('friend', 'john')
-    r.lpush('friend', 'mike')
 
-    return render_template("dashboard.html", data=r.lrange('friend', 0, -1))
+    r.lpush(
+        "things-data",
+        """{
+    "thingId": "38ED5BF550EE4CC6AD2BE9A7BE7111A4", 
+    "deviceId": "dcac5d23-cf60-44ff-82e8-b1fb4fd69efb", 
+    "sensorId": "f1ff9a46-86f7-47ef-befb-ccb8a76b90a8", 
+    "commandCapabilityId": "24c65333-6630-46d2-b4b8-69ca3f3786df", 
+    "coordinate": {"lat":"1.3050856285437102", "lng":"103.93210128266621"},
+    "zone":"East Coast Park Zone 1",
+    "status":"online"
+    }""",
+    )
 
+    r.lpush(
+        "things-data",
+        """{
+    "thingId": "38ED5BF550EE4CC6AD2BE9A7BE7111A4", 
+    "deviceId": "dcac5d23-cf60-44ff-82e8-b1fb4fd69efb", 
+    "sensorId": "f1ff9a46-86f7-47ef-befb-ccb8a76b90a8", 
+    "commandCapabilityId": "24c65333-6630-46d2-b4b8-69ca3f3786df", 
+    "coordinate": {"lat":"1.3883761", "lng":"103.9787106"},
+    "zone":"Changi Beach Zone 1",
+    "status":"offline"
+    }""",
+    )
 
+    r.lpush(
+        "alerts-data",
+        '{"event": "strong_wave", "datetime": "2021-10-12T22:02:29.735563Z", "zone":"East Coast Park Zone 1"}',
+    )
+
+    r.lpush(
+        "alerts-data",
+        '{"event": "wet_device", "datetime": "2021-10-12T21:02:29.735563Z", "zone":"East Coast Park Zone 1"}',
+    )
+
+    r.lpush(
+        "alerts-data",
+        '{"event": "strong_wave", "datetime": "2021-10-13T20:02:29.735563Z", "zone":"Changi Beach Zone 1"}',
+    )
+
+    list_things_json_strings = r.lrange("things-data", 0, -1)
+    things_data_list_dict = []
+    for json_string in list_things_json_strings:
+        things_data_list_dict.append(json.loads(json_string))
+
+    list_alerts_json_strings = r.lrange("alerts-data", 0, -1)
+    events_data_list_dict = []
+    for json_string in list_alerts_json_strings:
+        events_data_list_dict.append(json.loads(json_string))
+
+    return render_template(
+        "homepage.html",
+        things_data=things_data_list_dict,
+        events_data=events_data_list_dict,
+        things_data_json=json.dumps(things_data_list_dict),
+        events_data_json=json.dumps(events_data_list_dict),
+    )
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
---
+++ app/static/homepage.js
@@ -0,0 +1,172 @@
+function initial_load(){
+    // console.log(things_data_json)
+    console.log(events_data_json)
+    events_data=JSON.parse(events_data_json)
+    
+    const insert_alert=document.getElementById("insert_alerts_log")
+    let table_list=""
+    
+    let zones=[]
+    for(one_event of events_data){
+        zones.push(one_event.zone)
+        if(one_event.event=="wet_device"){
+            table_list=table_list+`   
+            <tr>
+                <td>
+                    Moisture detected on the device
+               </td>
+               <td>${one_event.zone}</td>
+               <td >${one_event.datetime}</td>
+               <td class="text-danger">
+                    High
+                </td>
+            </tr>`
+        }
+
+        if(one_event.event=="strong_wave"){
+            table_list=table_list+`   
+            <tr>
+                <td>
+                    Strong wave detected
+               </td>
+               <td>${one_event.zone}</td>
+               <td >${one_event.datetime}</td>
+               <td class="text-warning">
+                    Medium
+                </td>
+            </tr>`
+        }
+    }
+    insert_alert.innerHTML=table_list
+
+    const unique_zones = [...new Set(zones)];
+    const select_zone=document.getElementById("select_zone")
+    let select_zone_options=`<option id="All_Zones"value="All Zones">All Zones</option>`
+    for(zone of unique_zones){
+        select_zone_options=select_zone_options+`
+        <option id="${zone.replace(" ","_")}" value="${zone}">${zone}</option>
+        `
+    }
+    select_zone.innerHTML=select_zone_options
+}
+
+initial_load()
+
+function filter_alerts_by_zone(){
+    const selected_zone=document.getElementById("select_zone").value
+    display_alerts_log(selected_zone)
+}
+
+function filter_alerts_by_next_zone(){
+    const select_zone=document.getElementById("select_zone")
+    console.log(select_zone)
+    next_zone_value=document.getElementById(select_zone.value.replace(" ","_")).nextElementSibling.innerText
+    select_zone.value=next_zone_value
+    console.log(next_zone_value)
+    display_alerts_log(next_zone_value)
+}
+
+function filter_alerts_by_previous_zone(){
+    const select_zone=document.getElementById("select_zone")
+    console.log(select_zone)
+    next_zone_value=document.getElementById(select_zone.value.replace(" ","_")).previousElementSibling.innerText
+    select_zone.value=next_zone_value
+    console.log(next_zone_value)
+    display_alerts_log(next_zone_value)
+}
+
+function display_alerts_log(selected_zone){
+    const insert_alert=document.getElementById("insert_alerts_log")
+    // things_data=JSON.parse(things_data_json)
+    events_data=JSON.parse(events_data_json)
+    let table_list=""
+    for(one_event of events_data){
+        if(one_event.zone!= selected_zone && selected_zone!="All Zones"){
+            continue
+        }
+        if(one_event.event=="wet_device"){
+            table_list=table_list+`   
+            <tr>
+                <td>
+                    Moisture detected on the device
+               </td>
+               <td>${one_event.zone}</td>
+               <td >${one_event.datetime}</td>
+               <td class="text-danger">
+                    High
+                </td>
+            </tr>`
+        }
+
+        if(one_event.event=="strong_wave"){
+            table_list=table_list+`   
+            <tr>
+                <td>
+                    Strong wave detected
+               </td>
+               <td>${one_event.zone}</td>
+               <td >${one_event.datetime}</td>
+               <td class="text-warning">
+                    Medium
+                </td>
+            </tr>`
+        }
+    }
+    insert_alert.innerHTML=table_list
+}
+
+
+// Initialize and add the map
+function initMap() {
+    things_data=JSON.parse(things_data_json);
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 11,
+      center: {lat: 1.3521, lng: 103.8198},
+    });
+   
+    for(thing of things_data){
+        let svgMarker
+        if(thing.status=="online"){
+            svgMarker = {
+                path: "M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z",
+                fillColor: "green",
+                fillOpacity: 0.6,
+                strokeWeight: 0,
+                rotation: 0,
+                scale: 2,
+                anchor: new google.maps.Point(15, 30),
+                };
+        }else{
+            svgMarker = {
+                path: "M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z",
+                fillColor: "red",
+                fillOpacity: 0.6,
+                strokeWeight: 0,
+                rotation: 0,
+                scale: 2,
+                anchor: new google.maps.Point(15, 30),
+                };
+        }
+    
+        const contentString =`
+        <p>Location: ${thing.zone}</p>
+        <p class="${thing.status=='online'&&'text-success'|| 'text-danger'}">Device Status: ${thing.status}</p>`
+        const marker =new google.maps.Marker({
+            position: {lat:Number(thing.coordinate.lat), lng:Number(thing.coordinate.lng)},
+            icon: svgMarker,
+            map: map,
+          });
+        
+        const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        });
+
+        marker.addListener("click", () => {
+        infowindow.open({
+            anchor: marker,
+            map,
+            shouldFocus: false,
+        });
+        });
+    }
+  }
